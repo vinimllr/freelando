707b3492695172a077443b3a31c49846c789dd3f
Vinicius Rodrigues <viniciusrodriguesmuller@gmail.com>
2023-10-26 18:44:08 -0300

    first commit

--- src/components/Rodape/index.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Rodape = styled.footer`
-background: ${props => props.theme.cores.primarias.a};
-padding: ${props => props.theme.espacamentos.xl};
-color: ${props => props.theme.cores.branco};
-`
